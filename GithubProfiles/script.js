01b199bba720fcbb553446129473836e09083d2e
Antonio Hernández Santander <klee.collage@gmail.com>
2023-07-27 22:15:08 -0500

    update 27/07/2023
    Github profiles

--- GithubProfiles/script.js
+++ GithubProfiles/script.js
@@ -1,32 +1,31 @@
-const APIURL = 'http://api.github.com/users/';
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const main = document.getElementById('main')
+const APIURL = "http://api.github.com/users/";
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 async function getUser(userName) {
-
-    try {
-        const { data } = await axios(APIURL + userName)
-        createUserCard(data);
-        getRepos(userName);
-    } catch (error) {
-        if (error.response.status == 404) {
-            createErrorCard('There is no profile with this username')
-        }
+  try {
+    const { data } = await axios(APIURL + userName);
+    createUserCard(data);
+    getRepos(userName);
+  } catch (error) {
+    if (error.response.status == 404) {
+      createErrorCard("There is no profile with this username");
     }
+  }
 }
 
 async function getRepos(userName) {
-    try {
-        const { data } = await axios(APIURL + userName + "/repos?sort=created" )
-        addReposToCard(data);
-    } catch (error) {
-        createErrorCard('Problem fetching repos')
-    }
+  try {
+    const { data } = await axios(APIURL + userName + "/repos?sort=created");
+    addReposToCard(data);
+  } catch (error) {
+    createErrorCard("Problem fetching repos");
+  }
 }
 
 function createUserCard(user) {
-    const cardHTML = `
+  const cardHTML = `
         <div class="card">
             <div>
             <img
@@ -49,48 +48,43 @@ function createUserCard(user) {
             </div>
             </div>
         </div>
-        `
+        `;
 
-        main.innerHTML = cardHTML
+  main.innerHTML = cardHTML;
 }
 
 function createErrorCard(msg) {
-    cardHTML = `
+  cardHTML = `
         <div class="error">
             <h1>${msg}</h1>
         </div>
-    `
+    `;
 
-    main.innerHTML = cardHTML
+  main.innerHTML = cardHTML;
 }
 
 function addReposToCard(repos) {
-    const reposEl = document.getElementById('repos')
+  const reposEl = document.getElementById("repos");
 
-    repos
-    .slice(0,10)
-    .forEach(repo => {
-        const repoEl = document.createElement('a')
-        repoEl.classList.add("repo")
-        repoEl.href = repo.html_url
-        repoEl.target = "_blank"
-        repoEl.innerText = repo.name
+  repos.slice(0, 10).forEach((repo) => {
+    const repoEl = document.createElement("a");
+    repoEl.classList.add("repo");
+    repoEl.href = repo.html_url;
+    repoEl.target = "_blank";
+    repoEl.innerText = repo.name;
 
-        reposEl.appendChild(repoEl)
-    });
+    reposEl.appendChild(repoEl);
+  });
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const user = search.value
+  const user = search.value;
 
-    if (user) {
-        getUser(user)
+  if (user) {
+    getUser(user);
 
-        search.value=''
-    }
-})
-
-
-
+    search.value = "";
+  }
+});
